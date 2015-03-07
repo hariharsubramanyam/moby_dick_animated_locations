9d644f7b438e6890fecc60f65a73bbdd0962c8f0
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-07 15:45:16 -0500

    Convert csv to json

---
+++ util/clean_data.py
@@ -0,0 +1,21 @@
+import json
+lines = open("../data/md-locations.csv").readlines()
+
+result = []
+for line in lines[1:]:
+    split_line = line.replace("\n", "").split(",")
+    seq_no = int(split_line[0])
+    lat = float(split_line[1])
+    lon = float(split_line[2])
+    line_no = split_line[3]
+    page_no = split_line[4]
+    quote = ",".join(split_line[5:]).replace('"', "")
+    result.append({
+        "seq_no": seq_no,
+        "lat": lat,
+        "lon": lon,
+        "line_no": line_no,
+        "quote": quote
+        })
+
+open("../data/md-locations.json", "w").write(json.dumps(result))
